0aac535bbdcd7ca6b8415d2b21d5be63419b8533
AnisHamadouche <45787719+AnisHamadouche@users.noreply.github.com>
2023-02-27 14:25:57 +0000

    Update factor.m

--- utils/factor.m
+++ utils/factor.m
@@ -1,8 +1,8 @@
-function [L, U] = factor(A, rho)
+function [L, U] = factor(A, rho) % (H, rho, lambda_x, A, L_k, Mx)
     %A = double(A);
     [m, n] = size(A);
     if ( m >= n )    % if skinny
-       L = chol( double(A'*A + rho*eye(n)), 'lower' );
+       L = chol( double(A'*A + rho*eye(n)), 'lower' ); % rho*eye(n) => WLM-ADMM => lambda_x*rho*A'*L_k*A+lambda_x*Mx
     else            % if fat
        L = chol( double(eye(m) + 1/rho*(A*A')), 'lower' );
     end
